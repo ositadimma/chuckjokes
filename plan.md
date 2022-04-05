ea26bea152968682c989a78880bb2a95e897f9da
unknown <okolie.ositadimma@gmail.com>
2022-04-05 15:49:35 +0100

    first commit

---
+++ plan.md
@@ -0,0 +1,11 @@
+## planning
+#### identification of use cases
+#### identification of Api specifications
+
+## development
+#### index
+#### components
+
+## testing and QA
+## release
+
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <nav>
+        <div class="nav-links-container">
+            <div class="nav-link">
+                SO FUNKTIONIERTS
+            </div>
+            <div class="nav-link">
+                SONDERANGEBOTE     
+            </div>
+            <div class="nav-link">
+            <span><i>@</i></span><span>MEIN BEREICH</span> 
+            </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
